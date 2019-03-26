80a3a5ed1b0debf6a9b8ea0eae0c59a6ffd2dc10
Vikes2 <muczynski.pawel@gmail.com>
2019-03-26 02:23:13 +0100

    Added Ciphertext Autokey.

---
+++ 45/CAutokey.py
@@ -0,0 +1,143 @@
+class CAutokey:
+
+    def encrypt(self, polynomial, seed, file_name, file_name_output):
+        c = polynomial
+        f = open(file_name, 'rb')
+        f.seek(0, 2)
+        file_size = f.tell()
+        f2 = open(file_name_output, 'wb')
+        counter = 0
+        seek_counter = 1
+        exitloop = False
+        while 1:
+            if exitloop:
+                break
+            y = []
+            seek_value = seek_counter * -8192
+            if seek_value < -file_size:
+                temp_seek = f.tell() - 8192
+                f.seek(0)
+                if temp_seek > 0:
+                    byte_s = f.read(temp_seek)
+                else:
+                    byte_s = f.read(file_size)
+
+                exitloop = True
+
+            else:
+                f.seek(seek_value, 2)
+                byte_s = f.read(8192)
+            seek_counter += 1
+            if not byte_s:
+                break
+
+            input = []
+            # clean
+            for j in range(len(byte_s) - 1, -1, -1):
+                if byte_s[j] != "\n" and byte_s[j] != "\r" and byte_s[j] != "\r\n":
+                    input.append(byte_s[j])
+
+
+            reg = seed
+
+            for i in range(len(input)):
+                y = 0
+                # y to wynik xora skladnikow wieloianu (wewnetrzny xor)
+                for j in range(len(polynomial)):
+                    if c[j] == '1':
+                        y += int(reg[j])
+                # dodajemy skladnik x i xorujemy
+                y = (y + int(input[i])) % 2
+
+                #save y moze sie pojawic
+                f2.write(y.to_bytes(1, 'big'))
+
+                tmp = str(y)
+                #zatrzasniecie nowego stanu
+                for j in range(len(reg) -1):
+                    tmp += reg[j]
+                reg = tmp
+
+
+
+
+    def decrypt(self, polynomial, seed, file_name, file_name_output):
+        c = polynomial
+
+        f = open(file_name, 'rb')
+        f2 = open('temp_file.bin', 'wb')
+        counter = 0
+        while 1:
+            y = []
+
+            byte_s = f.read(8192)
+            if not byte_s:
+                break
+
+            input = []
+            # reverse and clean
+            for j in range(len(byte_s)):
+                if byte_s[j] != "\n" and byte_s[j] != "\r" and byte_s[j] != "\r\n":
+                    input.append(byte_s[j])
+
+
+            reg = seed
+            for i in range(len(input)):
+                y = 0
+                # y to wynik xora skladnikow wieloianu (wewnetrzny xor)
+                for j in range(len(polynomial)):
+                    if c[j] == '1':
+                        y += int(reg[j])
+                # dodajemy skladnik x i xorujemy
+                y = (y + int(input[i])) % 2
+
+                #save y moze sie pojawic
+                f2.write(y.to_bytes(1, 'big'))
+
+                tmp = str(input[i])
+                #zatrzasniecie nowego stanu
+                for j in range(len(reg) -1):
+                    tmp += reg[j]
+                reg = tmp
+
+        f.close()
+        f2.close()
+
+        self.reverse_file('temp_file.bin', file_name_output)
+
+        return
+
+    def reverse_file(self, input_file, output_file):
+            f = open(input_file, 'rb')
+            f.seek(0, 2)
+            file_size = f.tell()
+            f2 = open(output_file, 'wb')
+            counter = 0
+            seek_counter = 1
+            exitloop = False
+            while 1:
+                if exitloop:
+                    break
+                y = []
+                seek_value = seek_counter * -8192
+                if seek_value < -file_size:
+                    temp_seek = f.tell() - 8192
+                    f.seek(0)
+                    if temp_seek > 0:
+                        byte_s = f.read(temp_seek)
+                    else:
+                        byte_s = f.read(file_size)
+
+                    exitloop = True
+
+                else:
+                    f.seek(seek_value, 2)
+                    byte_s = f.read(8192)
+                seek_counter += 1
+                if not byte_s:
+                    break
+
+                for i in range(len(byte_s) -1, -1, -1):
+                    f2.write(byte_s[i].to_bytes(1, 'big'))
+            f.close()
+            f2.close()
--- 45/__main__.py
+++ 45/__main__.py
@@ -1,18 +1,18 @@
 from LFSRGenerator import LFSR
 from SSCGenerator import SSC
-# from CAutokey import CAutokey
+from CAutokey import CAutokey
 
 lfsr = LFSR()
 lfsr_blocks = lfsr.generate_blocks("1001", "1101")
 #print(lfsr_blocks)
 
 #
-ssc = SSC()
-encrypted = ssc.encrypt(lfsr_blocks, 'test2.bin', 'out.bin')
-decrypted = ssc.decrypt(lfsr_blocks, 'out.bin', 'test21.bin')
+# ssc = SSC()
+# encrypted = ssc.encrypt(lfsr_blocks, 'test2.bin', 'out.bin')
+# decrypted = ssc.decrypt(lfsr_blocks, 'out.bin', 'test21.bin')
 #
 #
-# cauto = CAutokey()
-# encrypted = cauto.encrypt("1001", "1101","aa", "bb")
-# decrypted = cauto.decrypt("1001", "1101","aa", "bb")
+cauto = CAutokey()
+encrypted = cauto.encrypt("1001", "1101",'test2.bin', 'out.bin')
+decrypted = cauto.decrypt("1001", "1101",'out.bin', 'test21.bin')
 
